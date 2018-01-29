e25f17cd2eacf440b53897186c89740bc43bb9b6
Kundan <kkishor@outlook.com>
2018-01-30 00:34:56 +0400

    Simple Server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { AboutPage } from './../pages/about/about';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -23,12 +27,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
